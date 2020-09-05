5cf1b50342ea0c8cc09afb820948995c9fcc9abf
Sai Sukesh <sukesh.sai04@gmail.com>
2020-09-05 17:23:57 +0530

    MainActivity Nav and ProfileActivity

--- app/src/main/java/com/example/iet_events/MainActivity.java
+++ app/src/main/java/com/example/iet_events/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.iet_events;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_constraint_layout) ConstraintLayout main_constraint_layout;
+    @BindView(R.id.bottom_bar) BottomAppBar bottom_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/example/iet_events/ui/ProfileActivity.java
+++ app/src/main/java/com/example/iet_events/ui/ProfileActivity.java
@@ -1,16 +1,87 @@
 package com.example.iet_events.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.iet_events.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.profile_image_view) CircleImageView profile_image_view;
+    @BindView(R.id.profile_name_input) TextInputEditText profile_name_input;
+    @BindView(R.id.update_button) Button update_button;
+
+    private Uri mainImageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ButterKnife.bind(this);
+
+        profile_image_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                    if (ContextCompat.checkSelfPermission(ProfileActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(ProfileActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                    }else{
+                        bringImageSelection();
+                    }
+                }else{
+                    bringImageSelection();
+                }
+            }
+        });
+
+        update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : Add users to database
+            }
+        });
+    }
+
+    private void bringImageSelection() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1,1)
+                .start(ProfileActivity.this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                mainImageURI = result.getUri();
+                profile_image_view.setImageURI(mainImageURI);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(ProfileActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
